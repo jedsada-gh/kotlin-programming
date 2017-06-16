cc1d9834ff232a1c34d01eff2613dcaf37339642
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-06-17 02:45:39 +0700

    scenario Metaprogramming.

---
+++ src/main/kotlin/metaprogramming/Transaction.kt
@@ -0,0 +1,32 @@
+package metaprogramming
+
+import java.lang.reflect.Type
+
+class Transaction(val id: Int, val amount: Double, var description: String) {
+    fun validate() {
+        if (amount > 10000) println("Transaction is to large")
+    }
+}
+
+fun introspectInstance(obj: Any) {
+    println("Class ${obj.javaClass.simpleName}")
+    println("Properties\n")
+    obj.javaClass.declaredFields.forEach {
+        println("${it.name} of type ${it.type}")
+    }
+    println("Functions\n")
+    obj.javaClass.declaredMethods.forEach {
+        println(it.name)
+    }
+}
+
+fun getType(obj: Type) {
+    println(obj.typeName + "\n")
+}
+
+fun main(args: Array<String>) {
+
+    getType(Transaction::class.java)
+
+    introspectInstance(Transaction(1, 200.0, "A simple transaction"))
+}
