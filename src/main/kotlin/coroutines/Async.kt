e430c74a2c43eabaa3e5d972da31779685c8353a
Jedsada Tiwongvorakul <jt@20scoops.com>
2017-06-17 23:43:17 +0700

    improvment code Async.kt

--- src/main/kotlin/coroutines/Async.kt
+++ src/main/kotlin/coroutines/Async.kt
@@ -3,27 +3,19 @@ package coroutines
 import kotlinx.coroutines.experimental.*
 import java.util.concurrent.CompletableFuture
 
-private fun startLongAsyncOperation(v: Int) {
-    CompletableFuture.supplyAsync {
-        Thread.sleep(1000)
-        "Result: $v"
-        println("$v")
-    }
+private fun startLongAsyncOperation(v: Int) = CompletableFuture.supplyAsync {
+    Thread.sleep(1000)
+    "Result: $v"
 }
 
 fun main(args: Array<String>) {
-    val feature = async(CommonPool) {
-        (1..5).map {
-            startLongAsyncOperation(it)
-        }
-    }
-
     runBlocking {
-        feature.await()
+        val feature = async(CommonPool) {
+            (1..5).map { startLongAsyncOperation(it) }
+        }.await().joinToString { it.get() + "\n" }
+
+        println("This before")
+        print(feature.replace(",", "").replace(" ", ""))
+        println("This after")
     }
-}
-
-// this is your first suspending function
-suspend fun doWorld() {
-
 }
